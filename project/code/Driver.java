4c011099babe7312d6ed57107e69b1942cdae8b0
ConwayJ18 <conway.jess18@gmail.com>
2018-03-12 14:56:41 -0500

    Add files via upload

---
+++ project/code/Driver.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Driver {
+
+    public static void main(String[] args)
+    {
+        boolean running = true;
+
+        while(running)
+        {
+            Scanner kbReader = new Scanner(System.in);
+            System.out.println("Please enter a number for testing. To quit, type 0: "); //Ask for test number
+            BigInteger testNumber = kbReader.nextBigInteger(); //Call that number "testNumber"
+            if(testNumber.equals(BigInteger.ZERO))
+            {
+                System.out.println("Program terminated.");
+                running=false;
+            } else
+            {
+                System.out.println(testNumber + " composite? " + !MillerRabin.isProbablePrime(testNumber, 40)); //Test if composite
+                System.out.println(testNumber + " is semi-prime? " + SemiPrime.isSemiPrime(testNumber)); //Test if semiPrime
+            }
+        }
+    }
+}
---
+++ project/code/README.txt
@@ -0,0 +1,15 @@
+COMP 460, Spring 2018 — Final Project
+
+Repo: https://github.com/ConwayJ18/comp460s18final/
+
+Contents:
+
+MillerRabin.java — Runs the MillerRabin primality test
+SemiPrime.java — Checks for semi-primality using MillerRabin then brute-force
+Driver.java — Can be used to run both tests at once, for as many numbers as necessary
+
+To run tests in Driver:
+1) Run Driver.java
+2) Type in any integer value, press enter
+2.a) This will return the results of both tests on that number
+3) Repeat as many times as desired, then type "0" to quit.
